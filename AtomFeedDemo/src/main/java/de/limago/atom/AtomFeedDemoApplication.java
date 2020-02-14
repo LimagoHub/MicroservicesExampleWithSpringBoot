3074b6519163240576aa8b6b51d06f26bf89beb6
limagoHub <limagoHub@limago.de>
2020-02-14 20:07:21 +0100

    test3

--- AtomFeedDemo/src/main/java/de/limago/atom/AtomFeedDemoApplication.java
+++ AtomFeedDemo/src/main/java/de/limago/atom/AtomFeedDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AtomFeedDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AtomFeedDemoApplication.class, args); 
+		SpringApplication.run(AtomFeedDemoApplication.class, args);
 	}
 
 }
