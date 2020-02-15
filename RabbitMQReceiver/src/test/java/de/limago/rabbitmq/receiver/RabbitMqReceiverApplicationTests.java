202d441e7e107724cdab647a2b8317d64282c48d
limagoHub <limagoHub@limago.de>
2020-02-15 17:08:35 +0100

    Docker-compose hinzugefuegt

---
+++ RabbitMQReceiver/src/test/java/de/limago/rabbitmq/receiver/RabbitMqReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package de.limago.rabbitmq.receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ RabbitMQSender/src/test/java/de/limago/rabbitmq/sender/RabbitMqSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.limago.rabbitmq.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
